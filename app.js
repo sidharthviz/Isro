f3d591571201191e12ca07e5b3fc14a597b9e9bf
Siddharth verma <990.siddharthverma@gmail.com>
2023-04-06 17:25:32 +0530

    wip

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-fetch('https://isro.vercel.app/api/spacecrafts')
-.then((data) => {
-    return data.json();  
-}).then((objectData) => {
-    console.log(objectData);
-    let space = "";
-    objectData.map((ele) => {
-        space = `<h1>${ele.spacecrafts[0]}</h1>`;
-    });
-    document.getElementById('isro').innerHTML = space
-})
+// // fetch(`https://isro.vercel.app/api/spacecrafts`)
+
+
+fetch("https://isro.vercel.app/api/spacecrafts")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data)
+    displaySpace(data)
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
+
+
+  function displaySpace(data){
+    const space = data.spacecrafts[4]
+    const spaceDiv = document.getElementById("space");
+    const spaceName = space.name
+    const heading = document.createElement("ul");
+    heading.innerHTML = spaceName;
+    spaceDiv.appendChild(heading);
+  }
